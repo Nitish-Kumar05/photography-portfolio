6cafa8d4506663dfd803e4e30b859453e112824a
Nitish-Kumar05 <mvnitishkumar@gmail.com>
2023-09-17 20:01:13 +0530

    added nav bar working on responsiveness

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <Hero
         heading="Clicks photography"
         message="Capturing moments, creating memories, one click at a time."
