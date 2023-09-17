3d11f1a533302fb224509443bb0663dd90bdb62c
Nitish-Kumar05 <mvnitishkumar@gmail.com>
2023-09-17 18:59:19 +0530

    worte hero section messages

--- app/page.js
+++ app/page.js
@@ -3,7 +3,10 @@ import Hero from "./components/Hero";
 export default function Home() {
   return (
     <div>
-      <Hero />
+      <Hero
+        heading="Clicks photography"
+        message="Capturing moments, creating memories, one click at a time."
+      />
     </div>
   );
 }
